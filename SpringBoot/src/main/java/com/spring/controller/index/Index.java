95cf70fed4c1cdb786266d2cb050b41cc2abfee3
zhangzq <614846076@qq.com>
2018-07-13 15:59:53 +0800

    登录

--- SpringBoot/src/main/java/com/spring/controller/index/Index.java
+++ SpringBoot/src/main/java/com/spring/controller/index/Index.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,6 +16,7 @@ import com.spring.common.dao.UserDao;
 import com.spring.common.dataSource.AlarmDataQuery;
 import com.spring.common.pojo.user.User;
 import com.spring.common.util.AnalysisSqlXmlUtil;
+import com.spring.common.util.MD5;
 
 @Controller
 @RequestMapping("/Index")
@@ -37,10 +39,11 @@ public class Index {
 //	}
 	
 	@RequestMapping("/login")
-	public String login(String username,String password,HttpServletRequest request,HttpServletResponse response) throws Exception{
+	public String login(String username,String password,HttpServletRequest request,HttpServletResponse response,Model model) throws Exception{
 		System.out.println("username:"+username);
 		System.out.println("password:"+password);
-		User user = userDao.validate(username, password);
+		String md5Password = MD5.getMD5Str(password);
+		User user = userDao.validate(username, md5Password);
 		boolean validate = false;
 		if(user!=null){
 			validate = true;
@@ -48,6 +51,7 @@ public class Index {
 		if(validate){
 			return "/Index/home";
 		}else{
+			model.addAttribute("status", "用户校验不通过，请检查账号或者密码");
 			return "/Index/login";
 		}
 		
