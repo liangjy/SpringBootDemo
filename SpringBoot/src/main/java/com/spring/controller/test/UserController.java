59e5b5e8dd152088b8a1797205c2352e1f69ac61
aideshuyu <960780049@qq.com>
2018-07-16 14:13:16 +0800

    提交增加Springboot支持jsp的功能并将页面成功显示出来

---
+++ SpringBoot/src/main/java/com/spring/controller/test/UserController.java
@@ -0,0 +1,20 @@
+package com.spring.controller.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/gotoUserJSP")
+    public String gotoUserJSP(Model model){
+        model.addAttribute("username" , "linch");
+        return "user";
+    }
+
+    @RequestMapping("/gotoHome")
+    public String gotoHome(){
+        return "IntelligentRoadTestV3/IntelligentRoadTestAnalysisV3";
+    }
+}
